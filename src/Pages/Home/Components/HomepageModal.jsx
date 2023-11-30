2119bf0344a42c9d57ef674a15fbd04f6e3403eb
alviasad0 <alviasad10.com>
2023-11-30 22:08:03 +0600

    final commit

--- src/Pages/Home/Components/HomepageModal.jsx
+++ src/Pages/Home/Components/HomepageModal.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 
 const HomepageModal = () => {
-    const [showModal, setShowModal] = useState(false);
+    const [, setShowModal] = useState(false);
      const navigate = useNavigate();
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setShowModal(true);
-      // Show SweetAlert modal after 10 seconds
+     
       Swal.fire({
         title: "Subscribe now for exclusive content!",
         imageUrl:
@@ -31,9 +31,7 @@ const HomepageModal = () => {
     }, 10000);
 
     return () => clearTimeout(timeoutId);
-  }, []); // Empty dependency array ensures useEffect runs only once on mount
-
-  // No need to render anything in the main component
+  }, []);
   return null;
 };
 
